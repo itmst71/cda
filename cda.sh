f52392c9b7680e87fce66314feebc72ae7c51f6d
itmst71 <itmst71@gmail.com>
2018-05-29 03:21:54 +0900

    Updated the version number

--- cda.sh
+++ cda.sh
@@ -30,7 +30,7 @@ _cda()
     #------------------------------------------------
     # app info
     declare -r APPNAME="cda"
-    declare -r VERSION="1.3.0 (2018-03-19)"
+    declare -r VERSION="1.4.0 (2018-05-28)"
 
     # override system variables
     local IFS=$' \t\n'
